ed6ddcd5d5b7fea6307359d988ff1532cabd57e5
D19vCaKcMe <76730629+D19vCaKcMe@users.noreply.github.com>
2020-12-29 17:50:22 +0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -11,18 +11,18 @@ module.exports = {
 
   run: async function (client, message, args) {
     const channel = message.member.voice.channel
-    if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
+    if (!channel)return sendError("error occured please join voice channel!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return sendError("There is nothing playing that I could skip for you.", message.channel);
+    if (!serverQueue)return sendError("an error occurred. There are no songs that can be stopped.", message.channel);
         if(!serverQueue.connection)return
 if(!serverQueue.connection.dispatcher)return
      if (serverQueue && !serverQueue.playing) {
       serverQueue.playing = true;
       serverQueue.connection.dispatcher.resume();
       let xd = new MessageEmbed()
-      .setDescription("▶ Resumed the music for you!")
-      .setColor("YELLOW")
-      .setTitle("Music has been Resumed!")
+      .setDescription("Skipped!")
+      .setColor("RANDOM")
+      .setTitle("!")
        
    return message.channel.send(xd).catch(err => console.log(err));
       
@@ -34,8 +34,8 @@ if(!serverQueue.connection.dispatcher)return
       } catch (error) {
         serverQueue.voiceChannel.leave()
         message.client.queue.delete(message.guild.id);
-        return sendError(`:notes: The player has stopped and the queue has been cleared.: ${error}`, message.channel);
+        return sendError(`the song has stopped and the queue has been cleared.: ${error}`, message.channel);
       }
-    message.react("✅")
+    message.react("<a:785428033972338709:791927788780650528>")
   },
 };

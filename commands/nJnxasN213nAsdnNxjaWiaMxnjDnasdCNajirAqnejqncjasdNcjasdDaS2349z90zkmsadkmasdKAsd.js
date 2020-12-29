9fdb32567e34364f9bb0d8c8089d9169d1d01736
D19vCaKcMe <76730629+D19vCaKcMe@users.noreply.github.com>
2020-12-29 17:45:01 +0700

    Update and rename nowplaying.js to nJnxasN213nAsdnNxjaWiaMxnjDnasdCNajirAqnejqncjasdNcjasdDaS2349z90zkmsadkmasdKAsd.js

--- commands/nJnxasN213nAsdnNxjaWiaMxnjDnasdCNajirAqnejqncjasdNcjasdDaS2349z90zkmsadkmasdKAsd.js
+++ commands/nJnxasN213nAsdnNxjaWiaMxnjDnasdCNajirAqnejqncjasdNcjasdDaS2349z90zkmsadkmasdKAsd.js
@@ -3,10 +3,10 @@ const sendError = require("../util/error")
 
 module.exports = {
   info: {
-    name: "nowplaying",
+    name: "nJnxasN213nAsdnNxjaWiaMxnjDnasdCNajirAqnejqncjasdNcjasdDaS2349z90zkmsadkmasdKAsd",
     description: "To show the music which is currently playing in this server",
     usage: "",
-    aliases: ["np"],
+    aliases: ["nJnxasN213nAsdnNxjaWiaMxnjDnasdCNajirAqnejqncjasdNcjasdDaS2349z90zkmsadkmasdKAsdasdmasdnjasfiasDAansjdAScaksxa"],
   },
 
   run: async function (client, message, args) {
@@ -14,12 +14,10 @@ module.exports = {
     if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
     let song = serverQueue.songs[0]
     let thing = new MessageEmbed()
-      .setAuthor("Now Playing", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
-      .setThumbnail(song.img)
-      .setColor("BLUE")
+      .setAuthor("Now Playing", "https://media.discordapp.net/attachments/788426369880293381/791925104559783966/-1_1_____.gif")
+      .setThumbnail()
+      .setColor("RANDOM")
       .addField("Name", song.title, true)
-      .addField("Duration", song.duration, true)
-      .addField("Requested by", song.req.tag, true)
       .setFooter(`Views: ${song.views} | ${song.ago}`)
     return message.channel.send(thing)
   },
